0b3fa31aaee7e1b09d8ea173c795a16750dbad52
jiangyou1 <351870651@qq.com>
2016-12-28 00:51:19 +0800

    add style theme

--- FirstBoold/app/src/main/java/sai/org/com/firstboold/fragment/FourGuildFragment.java
+++ FirstBoold/app/src/main/java/sai/org/com/firstboold/fragment/FourGuildFragment.java
@@ -1,108 +1,95 @@
 package sai.org.com.firstboold.fragment;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
-
+import sai.org.com.firstboold.MainActivity;
 import sai.org.com.firstboold.R;
 
 /**
  * Created by wujamie on 16/12/27.
  */
-
 public class FourGuildFragment extends GuideBaseFragment {
-    private ImageView ivReward;
-    private ImageView ivGold;
+    private static final float ZOOM_MAX = 1.3f;
+    private static final float ZOOM_MIN = 1.0f;
 
-    private Bitmap goldBitmap;
-    private boolean started;
+    private ImageView imgView_immediate_experience;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_guide_first, null);
-        ivReward = (ImageView) rootView.findViewById(R.id.iv_reward);
-        ivGold = (ImageView) rootView.findViewById(R.id.iv_gold);
-
-        goldBitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.icon_gold);
-        startAnimation();
-        return rootView;
+        View rooView = inflater.inflate(R.layout.fragment_guide_four, null);
+        imgView_immediate_experience = (ImageView) rooView.findViewById(R.id.imgView_immediate_experience);
+        imgView_immediate_experience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        return rooView;
     }
 
-    @Override
-    public void startAnimation() {
-        started = true;
-        //向下移动动画 硬币的高度*2+80
-        TranslateAnimation translateAnimation = new TranslateAnimation(0, 0, 0, goldBitmap.getHeight() * 2 + 80);
-        translateAnimation.setDuration(500);
-        translateAnimation.setFillAfter(true);
-        ivGold.startAnimation(translateAnimation);
+    public void playHeartbeatAnimation() {
+        /**
+         * 放大动画
+         */
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(new ScaleAnimation(ZOOM_MIN, ZOOM_MAX, ZOOM_MIN, ZOOM_MAX, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f));
+        animationSet.addAnimation(new AlphaAnimation(1.0f, 0.8f));
 
-        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+        animationSet.setDuration(500);
+        animationSet.setInterpolator(new AccelerateInterpolator());
+        animationSet.setFillAfter(true);
+
+        animationSet.setAnimationListener(new AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
             }
 
             @Override
-            public void onAnimationEnd(Animation animation) {
-                if (started) {
-                    ivReward.setVisibility(View.VISIBLE);
-                    //硬币移动动画结束开启缩放动画
-                    Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.reward_launcher);
-                    ivReward.startAnimation(anim);
-                    anim.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            //缩放动画结束 开启改变透明度动画
-                            AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
-                            alphaAnimation.setDuration(1000);
-                            ivReward.startAnimation(alphaAnimation);
-                            alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
-                                @Override
-                                public void onAnimationStart(Animation animation) {
-                                }
-
-                                @Override
-                                public void onAnimationRepeat(Animation animation) {
-                                }
-
-                                @Override
-                                public void onAnimationEnd(Animation animation) {
-                                    //透明度动画结束隐藏图片
-                                    ivReward.setVisibility(View.GONE);
-                                }
-                            });
-                        }
-                    });
-                }
+            public void onAnimationRepeat(Animation animation) {
             }
 
             @Override
-            public void onAnimationRepeat(Animation animation) {
+            public void onAnimationEnd(Animation animation) {
+                /**
+                 * 缩小动画
+                 */
+                AnimationSet animationSet = new AnimationSet(true);
+                animationSet.addAnimation(new ScaleAnimation(ZOOM_MAX, ZOOM_MIN, ZOOM_MAX, ZOOM_MIN, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f));
+                animationSet.addAnimation(new AlphaAnimation(0.8f, 1.0f));
+                animationSet.setDuration(600);
+                animationSet.setInterpolator(new DecelerateInterpolator());
+                animationSet.setFillAfter(false);
+                // 实现心跳的View
+                imgView_immediate_experience.startAnimation(animationSet);
             }
         });
+        // 实现心跳的View
+        imgView_immediate_experience.startAnimation(animationSet);
+    }
+
+
+    @Override
+    public void startAnimation() {
+        playHeartbeatAnimation();
     }
 
     @Override
     public void stopAnimation() {
-        started = false;//结束动画时标示符设置为false
-        ivGold.clearAnimation();//清空view上的动画
+
     }
 }
